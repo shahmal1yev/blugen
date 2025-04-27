a3e6e66aeb45b8c6dacc5e188e3ed003896b5392
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-04-27 18:16:53 +0400

    Add helper functions: implement toPascalCase and improve string utilities

--- src/helpers.php
+++ src/helpers.php
@@ -3,3 +3,10 @@
 function container(): Symfony\Component\DependencyInjection\ContainerBuilder {
     return \Blugen\Container::get();
 }
+
+function toPascalCase(string $input): string
+{
+    return implode('', array_map('ucfirst', explode(' ', strtolower(
+        preg_replace('/[-_]|(?<=[a-z])(?=[A-Z])/', ' ', $input)
+    ))));
+}
