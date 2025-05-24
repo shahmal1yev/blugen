6f2cff581ab869bd13fcaa6cfbebcaab25b26178
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-05-24 14:19:23 +0400

    chore(factory): register string type generator in definition factory

--- src/Service/Lexicon/V1/Factory/DefGeneratorFactory.php
+++ src/Service/Lexicon/V1/Factory/DefGeneratorFactory.php
@@ -6,12 +6,14 @@ use Blugen\Service\Lexicon\DefinitionInterface;
 use Blugen\Service\Lexicon\GeneratorInterface;
 use Blugen\Service\Lexicon\V1\DefGenerator\Field\ArrayGenerator;
 use Blugen\Service\Lexicon\V1\DefGenerator\Field\ObjectGenerator;
+use Blugen\Service\Lexicon\V1\DefGenerator\Field\StringGenerator;
 use Blugen\Service\Lexicon\V1\DefGenerator\Primary\ProcedureGenerator;
 use Blugen\Service\Lexicon\V1\DefGenerator\Primary\QueryGenerator;
 use Blugen\Service\Lexicon\V1\DefGenerator\Primary\RecordGenerator;
 use Blugen\Service\Lexicon\V1\DefGenerator\Primary\SubscriptionGenerator;
 use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Field\ArrayTypeDefinition;
 use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Field\ObjectTypeDefinition;
+use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Field\StringTypeDefinition;
 use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Primary\ProcedureTypeDefinition;
 use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Primary\QueryTypeDefinition;
 use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Primary\RecordTypeDefinition;
@@ -28,6 +30,7 @@ class DefGeneratorFactory
             'query' => new QueryGenerator(new QueryTypeDefinition($definition)),
             'procedure' => new ProcedureGenerator(new ProcedureTypeDefinition($definition)),
             'subscription' => new SubscriptionGenerator(new SubscriptionTypeDefinition($definition)),
+            'string' => new StringGenerator(new StringTypeDefinition($definition)),
             default => throw new \RuntimeException("Unexpected definition type '{$definition->type()}'")
         };
     }
