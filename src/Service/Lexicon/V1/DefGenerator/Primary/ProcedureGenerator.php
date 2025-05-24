fa58963c12eb1c564ed6301f3920e83da4d137a8
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-05-24 14:17:32 +0400

    feat(generator): support dynamic schema typing in procedure generation
    - Generates anonymous class for object schemas
    - Adds flexible union/ref typing with accurate method return signatures
    - Automatically resolves and imports referenced types

--- src/Service/Lexicon/V1/DefGenerator/Primary/ProcedureGenerator.php
+++ src/Service/Lexicon/V1/DefGenerator/Primary/ProcedureGenerator.php
@@ -4,10 +4,16 @@ namespace Blugen\Service\Lexicon\V1\DefGenerator\Primary;
 
 use Blugen\Service\Lexicon\GeneratorInterface;
 use Blugen\Service\Lexicon\ProcedureInterface;
+use Blugen\Service\Lexicon\V1\ComponentGenerator\Field\RefComponentGenerator;
 use Blugen\Service\Lexicon\V1\Factory\ComponentGeneratorFactory;
 use Blugen\Service\Lexicon\V1\Resolver\NamespaceResolver;
+use Blugen\Service\Lexicon\V1\Resolver\NsidResolver;
 use Blugen\Service\Lexicon\V1\TypeSpecificDefinition\Primary\ProcedureTypeDefinition;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Field\ObjectSchema;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Field\RefSchema;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Field\UnionSchema;
 use Nette\PhpGenerator\ClassType;
+use Nette\PhpGenerator\Literal;
 use Nette\PhpGenerator\PhpFile;
 use Nette\PhpGenerator\PhpNamespace;
 
@@ -32,9 +38,66 @@ class ProcedureGenerator implements GeneratorInterface
     {
         $this->class->addImplement(ProcedureInterface::class);
 
-        foreach($this->definition->input()?->schema()?->properties() as $property) {
-            ComponentGeneratorFactory::create($this->class, $property)->generate();
+        $schemaProperty = $this->class->addProperty("schema")
+            ->setType('object')
+            ->setPrivate();
+
+        $setSchemaMethod = $this->class->addMethod("setSchema")
+            ->setPublic()
+            ->setReturnType('object');
+
+        $getSchemaMethod = $this->class->addMethod("getSchema")
+            ->setPublic()
+            ->setReturnType('object');
+
+        $schema = $this->definition->input()?->schema();
+
+        if ($schema instanceof ObjectSchema) {
+            $anonClass = new ClassType();
+
+            foreach($schema?->properties() as $property) {
+                $generator = ComponentGeneratorFactory::create($anonClass, $property);
+
+                if ($generator instanceof RefComponentGenerator) {
+                    $a = 12;
+                }
+
+                $generator->generate();
+            }
+
+            $setSchemaMethod->addBody(new Literal("return \$this->schema = new class $anonClass;"));
+            $getSchemaMethod->addBody(new Literal("return \$this->schema;"));
+
+            return $this->file->__toString();
         }
+
+        $refs = [];
+
+        if ($schema instanceof RefSchema) {
+            $refs[] = $schema->ref();
+        }
+
+        if ($schema instanceof UnionSchema) {
+            $refs = array_merge($refs, $schema->refs());
+        }
+
+        $refs = array_map(function ($ref) {
+            $ref = NsidResolver::namespace($ref);
+
+            $this->file->addUse($ref);
+
+            return $ref;
+        }, $refs);
+
+        $setSchemaMethod->setReturnType("self")
+            ->addBody(new Literal("\$this->schame = \$schema;\nreturn \$this;"))
+            ->addParameter("schema")
+            ->setType(implode("|", $refs));
+
+        $getSchemaMethod->addBody(new Literal("return \$this->schame;"))
+            ->setReturnType(implode("|", $refs));
+
+        $schemaProperty->setType(implode("|", $refs));
 
         return $this->file->__toString();
     }
