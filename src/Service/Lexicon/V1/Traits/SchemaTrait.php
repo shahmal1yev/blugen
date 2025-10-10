c43ec8779a9bf4e2cb1ea43a82c26fc31d8e7dce
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-10-10 23:28:50 +0400

    Add arrayable schema/definition traits

---
+++ src/Service/Lexicon/V1/Traits/SchemaTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Blugen\Service\Lexicon\V1\Traits;
+
+use Blugen\Service\Lexicon\SchemaInterface;
+
+trait SchemaTrait
+{
+    public function schema(): array
+    {
+        $propertyExists = property_exists($this, 'schema');
+
+        if ($propertyExists && $this->schema instanceof SchemaInterface) {
+            return $this->schema->schema();
+        }
+
+        if ($propertyExists && is_array($this->schema)) {
+            return $this->schema;
+        }
+
+        throw new \LogicException(sprintf(
+            "%s::\$schema must be an array or implement %s",
+            self::class,
+            SchemaInterface::class
+        ));
+    }
+}
