cf5492fe5ed9440914cea8bd05ff4b23a30ad087
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-10-13 00:13:26 +0400

    Test(Input/Output): Add unit tests for Input and Output schemas

---
+++ tests/Unit/Service/Lexicon/V1/TypeSpecificSchema/Support/OutputSchemaTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Blugen\Tests\Unit\Service\Lexicon\V1\TypeSpecificSchema\Support;
+
+use Blugen\Service\Lexicon\V1\Schema;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Support\InputSchema;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Support\OutputSchema;
+use Blugen\Tests\TestCase;
+use TypeError;
+
+class OutputSchemaTest extends TestCase
+{
+    public function test_type_returns_expected_value(): void
+    {
+        $schema = new OutputSchema(new Schema([]));
+
+        $expected = 'output';
+        $actual = $schema->type();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function test_description_returns_expected_value(): void
+    {
+        $schema = new OutputSchema(new Schema([
+            'description' => 'An example description.',
+        ]));
+
+        $expected = 'An example description.';
+        $actual = $schema->description();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function test_description_is_optional(): void
+    {
+        $schema = new OutputSchema(new Schema([
+            // missing description
+        ]));
+
+        $this->assertNull($schema->description());
+    }
+
+    public function test_encoding_returns_expected_value(): void
+    {
+        $schema = new OutputSchema(new Schema([
+            'encoding' => 'application/json',
+        ]));
+
+        $this->assertSame('application/json', $schema->encoding());
+    }
+
+    public function test_encoding_is_required(): void
+    {
+        $schema = new OutputSchema(new Schema([
+            // missing encoding
+        ]));
+
+        $this->expectException(TypeError::class);
+
+        $schema->encoding();
+    }
+
+    public function test_schema_returns_expected_value(): void
+    {
+        $schema = ['type' => 'ref', 'ref' => 'com.example.schema'];
+        $OutputSchema = new OutputSchema(new Schema(['schema' => $schema]));
+
+        $this->assertSame($schema, $OutputSchema->schema());
+    }
+
+    public function test_schema_is_required(): void
+    {
+        $schema = new OutputSchema(new Schema([
+            // missing schema
+        ]));
+
+        $this->expectException(TypeError::class);
+
+        $schema->schema();
+    }
+
+    public function test_toArray_works_properly(): void
+    {
+        $expected = [
+            'description' => 'An example description.',
+            'encoding' => 'application/json',
+            'schema' => [
+                'type' => 'union',
+                'refs' => [
+                    'com.example.schema',
+                    'com.test.another.schema',
+                    'tools.example.schema',
+                ]
+            ]
+        ];
+
+        $schema = new OutputSchema(new Schema($expected));
+
+        $this->assertSame($expected, $schema->toArray());
+    }
+
+    public function test_parameters_returns_expected_value(): void
+    {
+        $params = [
+            'required' => ['field1', 'field2'],
+            'properties' => [
+                [
+                    'type' => 'string',
+                    'format' => 'did',
+                ],
+                [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'integer',
+                        'minimum' => 30,
+                        'maximum' => 100,
+                    ]
+                ]
+            ],
+        ];
+
+        $schema = new InputSchema(new Schema([
+            'parameters' => $params,
+        ]));
+
+        $this->assertSame($params, $schema->parameters());
+    }
+
+    public function test_parameters_is_optional(): void
+    {
+        $schema = new InputSchema(new Schema([
+            // missing parameters
+        ]));
+
+        $this->assertNull($schema->parameters());
+    }
+}
