0360523a989e71967f4682e8d9dc467041816901
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-10-13 13:15:45 +0400

    Test(Schema/Primary): Add unit tests for primary type schemas

---
+++ tests/Unit/Service/Lexicon/V1/TypeSpecificSchema/Primary/RecordSchemaTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Blugen\Tests\Unit\Service\Lexicon\V1\TypeSpecificSchema\Primary;
+
+use Blugen\Service\Lexicon\SchemaInterface;
+use Blugen\Service\Lexicon\V1\Schema;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Field\ObjectSchema;
+use Blugen\Service\Lexicon\V1\TypeSpecificSchema\Primary\RecordSchema;
+use Blugen\Tests\TestCase;
+use Blugen\Tests\Unit\Traits\WithArrayableTestTrait;
+use Blugen\Tests\Unit\Traits\WithGetTestTrait;
+use Blugen\Tests\Unit\Traits\WithSchema;
+use Blugen\Tests\Unit\Traits\WithSchemaTestTrait;
+
+class RecordSchemaTest extends TestCase
+{
+    use WithSchema;
+    use WithGetTestTrait;
+    use WithSchemaTestTrait;
+    use WithArrayableTestTrait;
+
+    public function test_key_returns_expected_value(): void
+    {
+        $schema = $this->schema([
+            'key' => 'tid'
+        ]);
+
+        $this->assertSame('tid', $schema->key());
+        $this->assertSame(['key' => 'tid'], $schema->toArray());
+    }
+
+    public function test_key_is_required(): void
+    {
+        $schema = $this->schema([
+            // missing key
+        ]);
+
+        $this->expectException(\TypeError::class);
+        $schema->key();
+    }
+
+    public function test_record_returns_expected_value(): void
+    {
+        $schema = $this->schema([
+            'record' => [
+                'type' => 'object',
+                'properties' => [
+                    'field1' => [],
+                    'field2' => [],
+                ]
+            ]
+        ]);
+
+        $this->assertInstanceOf(ObjectSchema::class, $objectSchema = $schema->record());
+        $this->assertSame([
+            'type' => 'object',
+            'properties' => [
+                'field1' => [],
+                'field2' => [],
+            ]
+        ], $objectSchema->toArray());
+    }
+
+    public function test_record_is_required(): void
+    {
+        $schema = $this->schema([
+            // missing record
+        ]);
+
+        $this->expectException(\TypeError::class);
+        $schema->record();
+    }
+
+    private function schema(array $content): RecordSchema
+    {
+        return new RecordSchema(new Schema($content));
+    }
+}
